5aeb16c196cfae869bcefc4650cf302b565da68b
dinakar777 <dinakarchennupati777@gmail.com>
2022-05-25 18:13:27 +0530

    add tailwind & coderefactoring

--- add-your-ideas/src/components/Header.tsx
+++ add-your-ideas/src/components/Header.tsx
@@ -2,14 +2,13 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="absolute left-0 right-0 flex justify-center gap-2 content-center z-[2000]">
       <img
         src="https://pbs.twimg.com/media/D8Dp0c5WkAAkvME.jpg"
-        width={40}
-        height={40}
         alt="Idea"
+        className="rounded-full object-cover w-8 h-8 lg:w-16 lg:h-16 ml-2 self-center"
       />
-      <h2>Create your Own Ideas</h2>
+      <h2 className="font-[Gentium_Basic] text-4xl text-center lg:text-left lg:text-6xl font-bold">Create your Own Ideas</h2>
     </header>
   );
 };
--- add-your-ideas/src/components/SlideShow.tsx
+++ add-your-ideas/src/components/SlideShow.tsx
@@ -1,60 +1,94 @@
-import { Carousel } from "react-bootstrap";
+import "tw-elements";
 
 const SlideShow = () => {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/6QgfpGJc/ideabg2.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption className="caption">
-          <h3>
-            {" "}
-            To sit on an idea or fail to act on a goal is not really
-            goal-setting, but wishful thinking.
-          </h3>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/QdDPjwkC/My-project.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption className="caption">
-          <h3>
-            Let your ideas and thoughts give you inspiration. All creativity
-            comes from your imagination - you first imagine and then you create.
-          </h3>
-          <br/>
-          <br/>
-          <br/>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.postimg.cc/3rPVcdkg/My-project-1.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption className="caption">
-          <h3>
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide relative"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner relative w-full overflow-hidden">
+        <div className="relative carousel-item active float-left w-full">
+          <img
+            src="https://i.postimg.cc/6QgfpGJc/ideabg2.jpg"
+            className="block w-full"
+            alt="Wild Landscape"
+          />
+          <p className="lg:text-3xl absolute top-1/3 bottom-1/2 left-10 lg:top-40  lg:left-20 text-white">
             To make great ideas a reality, we must act, experiment, fail, adapt,
-            and learn on a daily basis.
-          </h3>
-          <br/>
-          <br/>
-          <br/>
-          <br/>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+            and learn on a daily basis
+          </p>
+        </div>
+        <div className="relative carousel-item float-left w-full">
+          <img
+            src="https://i.postimg.cc/QdDPjwkC/My-project.jpg"
+            className="block w-full"
+            alt="Camera"
+          />
+          <p className="lg:text-3xl absolute top-1/3 bottom-1/2 left-10 lg:top-40  lg:left-20 text-white">
+            Let your ideas and thoughts give you inspiration. All creativity
+            come from your imagination - you first imagine and then you create
+          </p>
+        </div>
+        <div className="relative carousel-item float-left w-full">
+          <img
+            src="https://i.postimg.cc/3rPVcdkg/My-project-1.jpg"
+            className="block w-full"
+            alt="Exotic Fruits"
+          />
+          <p className="lg:text-3xl absolute top-1/3 bottom-1/2 left-10 lg:top-40  lg:left-20 text-white">
+            To sit on an idea or fail to act on a goal, is not really
+            goal-setting, but wishful thinking.
+          </p>
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span
+          className="carousel-control-prev-icon inline-block bg-no-repeat"
+          aria-hidden="true"
+        ></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span
+          className="carousel-control-next-icon inline-block bg-no-repeat"
+          aria-hidden="true"
+        ></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 };
 
